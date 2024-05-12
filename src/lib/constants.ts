eed08554f4790ea5c180e10a74970ea6f193aa6f
Antonov-S <anto1nov1@gmail.com>
2024-05-12 21:32:43 +0300

    Adding resources for Info section

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -8,6 +8,9 @@ import ic_language from "../../public/ic_language.png";
 import face from "../../public/face.png";
 import laptop from "../../public/laptop.png";
 import wand from "../../public/wand.png";
+import info_pic_1 from "../../public/info_pic_1.svg";
+import info_pic_2 from "../../public/info_pic_2.svg";
+import info_pic_3 from "../../public/info_pic_3.svg";
 
 export const NAV_LOGO = roooby;
 export const HOME_DIAGRAM = diagram;
@@ -16,6 +19,9 @@ export const TWITTER_ICON = twitter;
 export const FACEBOOK_ICON = facebook;
 export const LINKEDIN_ICON = linkedin;
 export const LANGUAGE_ICON = ic_language;
+export const INFO_PIC_1 = info_pic_1;
+export const INFO_PIC_2 = info_pic_2;
+export const INFO_PIC_3 = info_pic_3;
 
 export const NAV_MENU_ITEMS = [
   { name: "Product", path: "/products" },
@@ -124,4 +130,47 @@ export const FOOTER_LINKS_DATA = [
   { name: "Terms of Service", href: "#", target: "_self" },
   { name: "Privacy Policy", href: "#", target: "_self" },
   { name: "Cookies", href: "#", target: "_self" }
+];
+
+export const info = [
+  {
+    id: 1,
+    tag: "sales",
+    title: "Increase company revenue up to 65%",
+    description:
+      "Automate your sales, marketing, and service in one platform. Avoid data leaks and enable consistent messaging.",
+    points: [
+      "Close more deals with single-page contact management",
+      "Enjoy one-click calling, call scripts and voicemail automation",
+      "Track stages and milestones of your deals to keep the sales process on track"
+    ],
+    image: INFO_PIC_1
+  },
+  {
+    id: 2,
+    tag: "marketing",
+    title: "Marketing",
+    description:
+      "Market like the most successful companies in the world with Agile CRM's marketing automation software.",
+    points: [
+      "Marketing workflows with an easy drag-and-drop designer",
+      "Use our templates to create high converting, mobile responsive landing pages",
+      "Integrate social media easily into your marketing campaigns & contact views"
+    ],
+    image: INFO_PIC_2,
+    cta: "Automate your sales & marketing"
+  },
+  {
+    id: 3,
+    tag: "service",
+    title: "Help Desk Software",
+    description:
+      "Great customer support separates decent companies from excellent companies. A positive customer support experience is a crucially important piece of the customer journey.",
+    points: [
+      "Resolve every issue in the shortest possible time with powerful ticketing features",
+      "Categorize tickets according to the issue and route them to the appropriate group (e.g. sales or support)",
+      "Enjoy at-a-glance views of important data—such as ticket priority—on your dashboard"
+    ],
+    image: INFO_PIC_3
+  }
 ];
